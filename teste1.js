de4a547b12d4867d4300b981a7748f477b33697a
Pedro558 <pedroafonso109@gmail.com>
2018-08-08 12:53:39 -0300

    Primeiro Commit

---
+++ teste1.js
@@ -0,0 +1,17 @@
+alert('Voc\u00ea est\u00e1 em uma trap maculmunada pelo javascript')
+
+//Comentário de uma linha
+aaaa
+/*Comentário de + de 1x linha*/
+
+//String
+var texto = "Isso é um texto"
+
+//Float
+var numFracionado = 7.55;
+
+//Int
+var numInteiro = 8;
+
+//Boolean
+var teste = true; //poderia ser false tmb
